de17f3919f734bf925823e0846faf1edd227e758
vay <3484466304@qq.com>
2023-08-15 17:25:38 +0800

    完善布局，为组件Item添加选中、悬浮动画以及通过滚轮修改变量的功能。

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,61 @@
 <script setup lang="ts">
   import {onMounted} from 'vue';
+  import Item from './components/Item.vue';
   onMounted(()=>{
     const body = document.getElementsByTagName('body')[0] as HTMLBodyElement
     body.style.minWidth = body.clientWidth + 'px'
     body.style.minHeight = body.clientHeight + 'px'
   })
-  import Item from './components/Item.vue';
+  function getImgs(arr:Array<string>,group:string){
+    let result:Array<any> = []
+    for(let item of arr){
+      result.push(new URL('./assets/' + group + '/Icon_' + item + '.webp', import.meta.url).href)
+    }
+    return result
+  }
+  const BasicItems =  [
+    'Food_Canned','Raw_Food','Vegetables','Meal','Bandages','Medicine',
+    'Herbal_Medicine','Cigarette','Qualityrollup','Cigarettesrollup','Coffee','Tobacco1',
+    'Tobacco2','Herbs','Fertilizer','Medingredients']
+  let BasicItemsImgs:Array<any> = getImgs(BasicItems,'BasicItems')
+  
+  const Materials = [
+    'Components','Wood','Parts','Electric_Parts','Sugar','Water',
+    'Pure_Alcohol','Broken_Guitar','Helmet','Damaged_Vest','Conduct_Pass','Snow',
+    'Weapon_Parts','Shells','Gunpowder','Broken_Pistol','Broken_Shotgun','Broken_Assault_Rifle'
+  ]
+  let MaterialsImgs:Array<any> = getImgs(Materials,'Materials')
+  
+  const Consumables = [
+    'Filter','Fuel'
+  ]
+  let ConsumablesImgs:Array<any> = getImgs(Consumables,'Consumables')
+
+  const Other = [
+    'Books','Heat_Lamp','Jewel','Mixer','Moonshine','Snowman','Thermo_Regulator'
+  ]
+  let OtherImgs:Array<any> = getImgs(Other,'Other')
+
+  const Equipment = [
+    'Crowbar','Lock_Pick','Shovel','Saw','Hatchet'
+  ]
+  let EquipmentImgs:Array<any> = getImgs(Equipment,'Equipment')
+
+  const Armor = [
+    'Helmet','Military_Vest'
+  ]
+  let ArmorImgs:Array<any> = getImgs(Armor,'Armor')
+
+  const Weapons = [
+    'Bullet','Knife','Pistol','Rifle1','Rifle2','Shotgun'
+  ]
+  let WeaponsImgs:Array<any> = getImgs(Weapons,'Weapons')
+
+  const HouseholdItems = [
+    'Bed','Sofa','Chair','Heater','Board_Up','Reinforced_Door',
+    'Radio','Guitar','Toy_Box','Boll','Jumping_Rope','Swing'
+  ]
+  let HouseholdItemsImgs = getImgs(HouseholdItems,'Household Items')
 </script>
 
 <template>
@@ -18,11 +68,55 @@
           <span>后一天</span>
         </div>
       </div>
-      <div class="row" style="height: 80%;">
-        <div style="height: 100%;display: flex;padding: 70px 30px 30px 60px;;flex-direction: column;justify-content: flex-start;align-items: center;">
-          <div class="sets" style="width: 100%;display: flex;flex-direction: column;justify-content: flex-start;align-items: flex-start;color:white">
+      <div id="scroll" class="row" style="height: 80%;overflow-y: scroll;">
+        <div style="height: auto;display: flex;padding: 50px 20px 100px 60px;flex-direction: column;justify-content: flex-start;align-items: center;">
+          <div class="group">
             <span>基础物品</span>
-            <Item></Item>
+            <div>
+              <Item v-for="index in 16" :img="BasicItemsImgs[index-1]" ></Item>
+            </div>
+          </div>
+          <div class="group">
+            <span>材料</span>
+            <div>
+              <Item v-for="index in 18" :img="MaterialsImgs[index-1]"></Item>
+            </div>
+          </div>
+          <div class="group">
+            <span>消耗品</span>
+            <div>
+              <Item v-for="index in 2" :img="ConsumablesImgs[index-1]"></Item>
+            </div>
+          </div>
+          <div class="group">
+            <span>其他</span>
+            <div>
+              <Item v-for="index in 7" :img="OtherImgs[index-1]"></Item>
+            </div>
+          </div>
+          <div class="group">
+            <span>工具</span>
+            <div>
+              <Item v-for="index in 5" :img="EquipmentImgs[index-1]"></Item>
+            </div>
+          </div>
+          <div class="group">
+            <span>防具</span>
+            <div>
+              <Item v-for="index in 2" :img="ArmorImgs[index-1]"></Item>
+            </div>
+          </div>
+          <div class="group">
+            <span>武器</span>
+            <div>
+              <Item v-for="index in 6" :img="WeaponsImgs[index-1]"></Item>
+            </div>
+          </div>
+          <div class="group">
+            <span>家具</span>
+            <div>
+              <Item v-for="index in 12" :img="HouseholdItemsImgs[index-1]"></Item>
+            </div>
           </div>
         </div>
       </div>
@@ -38,7 +132,27 @@
     font-weight: 550;
     font-size: x-large;
   }
-  .sets *{
+  #scroll::-webkit-scrollbar {
+    width: 0 !important
+  }
+  .group{
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    color:white;
     margin: 4px;
+    > div{
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: flex-start;
+      flex-wrap: wrap;
+      > *{
+        margin: 5px;
+      }
+    }
   }
 </style>
--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,56 +1,174 @@
 <template>
-    <div class="edge" @mouseover="onMouseOver" @mouseleave="onMouseLeave" style="height: 80px;width: 80px;padding: 4px;display: flex;position: relative;;justify-content: center;align-items: center;">
-        <div style="position: absolute;height: 100%;width: 50px;background-color: rgb(11,12,7);">
+    <div style="display: flex;flex-direction: column;justify-content: flex-start;align-items: center;">
+        <div class="edge" ref="edgeRef" @click="onClickItem" style="height: 80px;width: 80px;padding: 4px;display: flex;position: relative;;justify-content: center;align-items: center;margin-bottom: 6px;">
+            <div style="position: absolute;height: 100%;width: 50px;background-color: rgb(11,12,7);">
 
-        </div>
-        <div style="position: absolute;height: 50px;width: 100%;background-color: rgb(11,12,7);">
+            </div>
+            <div style="position: absolute;height: 50px;width: 100%;background-color: rgb(11,12,7);">
 
-        </div>
-        <div class="edge" add style="position: absolute;height: 8px;width: 8px;z-index: 2;right: 0;top: 0;">
+            </div>
+            <div>
+                <div :ref="plusRef" style="position: absolute;height: 8px;width: 8px;z-index: 2;right: 0;top: 0;">
 
-        </div>
-        <div class="edge" add style="position: absolute;height: 8px;width: 8px;z-index: 2;right: 0;top: 14px;">
+                </div>
+                <div :ref="plusRef" style="position: absolute;height: 8px;width: 8px;z-index: 2;right: 0;top: 14px;">
 
-        </div>
-        <div class="edge" add style="position: absolute;height: 8px;width: 8px;z-index: 2;right: 14px;top: 0;">
+                </div>
+                <div :ref="plusRef" style="position: absolute;height: 8px;width: 8px;z-index: 2;right: 14px;top: 0;">
 
-        </div>
-        <div class="edge" add style="position: absolute;height: 8px;width: 8px;z-index: 2;right: 14px;top: 14px;">
+                </div>
+                <div :ref="plusRef" style="position: absolute;height: 8px;width: 8px;z-index: 2;right: 14px;top: 14px;">
 
-        </div>
-        <div class="edge" add style="position: absolute;height: 22px;width: 4px;z-index: 2;right: 18px;top: 0;">
+                </div>
+                <div :ref="plusRef" style="position: absolute;height: 22px;width: 4px;z-index: 2;right: 18px;top: 0;">
 
-        </div>
-        <div class="edge" add style="position: absolute;height: 4px;width: 22px;z-index: 2;right: 0;top: 18px;">
+                </div>
+                <div :ref="plusRef" style="position: absolute;height: 4px;width: 22px;z-index: 2;right: 0;top: 18px;">
 
-        </div>
-        <div class="edge" add style="position: absolute;height: 4px;width: 22px;z-index: 2;right: 0;top: 0;">
+                </div>
+                <div :ref="plusRef" style="position: absolute;height: 4px;width: 22px;z-index: 2;right: 0;top: 0;">
 
-        </div>
-        <div class="edge" add style="position: absolute;height: 22px;width: 4px;z-index: 2;right: 0;top: 0;">
+                </div>
+                <div :ref="plusRef" style="position: absolute;height: 22px;width: 4px;z-index: 2;right: 0;top: 0;">
 
+                </div>
+            </div>
+            <div style="height: 100%;width: 100%;background-color: rgb(11,12,7);z-index: 1;">
+                <img style="height: 100%;width: 100%;"  :src="props.img"/>
+            </div>
         </div>
-        <div style="height: 100%;width: 100%;background-color: rgb(11,12,7);z-index: 1;">
-            
-        </div>
+        <input v-model="count" @mousewheel.prevent="onMouseWheel" @click="onClickNumber" @keydown="onKeyDown"/>
     </div>
 </template>
 <script lang="ts" setup>
-    function onMouseOver(){
-        for(let el of document.getElementsByClassName('edge')){
-            const El = el as HTMLDivElement
-            El.style.backgroundColor = 'rgb(255,127,2)'
+    import {ref,watch} from 'vue'
+    let edgeRef = ref(null)
+    let plusRefs = ref<Array<HTMLDivElement>>([])
+    function plusRef(el:any){
+        if(el !== null)
+            plusRefs.value.push(el as HTMLDivElement)
+    }
+    function onClickItem(){
+        const edgeEl = edgeRef.value as unknown as HTMLDivElement
+        if(edgeEl.getAttribute('selected') !== null){
+            const keyframes = [
+                {transform: 'scale(1.05)'},
+                {transform: 'scale(1)'}
+            ]
+            const options = <KeyframeAnimationOptions>{
+                // 动画执行次数
+                iterations: 2,
+                // 动画开始时间点
+                iterationStart: 0,
+                // 动画开始之前的延迟
+                delay: 0,
+                // 动画结束之后的延迟
+                endDelay: 0,
+                // 动画是否在下一周期逆向播放
+                direction: 'alternate',
+                // 动画时长
+                duration: 130,
+                // 动画前后保持的状态
+                fill: 'both',
+                // 动画缓动类型
+                easing: 'linear',
+            };
+            edgeEl.animate(keyframes,options)
+            if(count.value > 0)
+                count.value -= 1
+        }else{
+            for(let sle of document.querySelectorAll('[selected]')){
+                sle.removeAttribute('selected')
+            }
+            for(let ple of plusRefs.value){
+                ple.setAttribute('selected','true')
+            }
+            edgeEl.setAttribute('selected','true')
         }
     }
-    function onMouseLeave(){
-        for(let el of document.getElementsByClassName('edge')){
-            const El = el as HTMLDivElement
-            El.style.backgroundColor = 'rgb(255,255,255)'
-        }
+    let count = ref(0)
+    watch(count,(newvalue)=>{
+        if(typeof newvalue === "string" && (newvalue as string).length === 0)
+            count.value = 0
+        if(newvalue > 999)
+            count.value = 999
+    })
+    function onMouseWheel(event:WheelEvent){
+        if(event.deltaY > 0 && count.value === 0)
+            count.value = 0
+        else if(event.deltaY > 0)
+            count.value -= 1
+        else
+            count.value += 1
     }
+    function onClickNumber(event:MouseEvent){
+        const edgeEl = edgeRef.value as unknown as HTMLDivElement
+        for(let sle of document.querySelectorAll('[selected]')){
+            sle.removeAttribute('selected')
+        }
+        for(let ple of plusRefs.value){
+            ple.setAttribute('selected','true')
+        }
+        edgeEl.setAttribute('selected','true');
+        (event.target as HTMLInputElement).select();
+    }
+    function onKeyDown(event: KeyboardEvent){
+        const target = event.target as HTMLDivElement
+        
+        if(event.key === 'Enter')
+            target.blur()
+        let isNum = /[0-9]/
+        if(!isNum.test(event.key) && event.key.length <= 1)
+            event.preventDefault();
+        
+    }
+    const props = defineProps<{
+        img:string
+    }>()
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+    img{
+        -webkit-user-drag: none;
+        user-select: none;
+    }
+    input{
+        height: 1em;
+        width: 36px;
+        text-align: center;
+        color: aliceblue;
+        font-weight: 550;
+        font-size: medium;
+        white-space: nowrap;
+        background-color: rgb(11,12,7,0);
+        outline: none;
+        overflow: hidden;
+        border-left: none;
+        border-right: none;
+        border-top: none;
+        border-bottom-color: rgb(11,12,7,0);
+    }
+    input:hover{
+        border-bottom-color: aliceblue;
+    }
+    @keyframes anim-hover{
+        0%{
+            scale: (1);
+        }
+        100%{
+            scale: (1.05);
+        }
+    }
     .edge{
         background-color: rgb(255,255,255);
+        &:hover:not([selected]){
+            background-color: rgb(255,127,2);
+            animation: anim-hover 130ms linear 0s 1 normal both;
+        }
+        &[selected]{
+            transform: scale(1.05);
+        }
+    }
+    [selected]{
+        background-color: rgb(255,127,2);
     }
 </style>
